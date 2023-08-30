f0be61c2e61b282a64c5214e3fddf86e85b6ea78
Arjun Singh <55157623+arjunsinghbazal@users.noreply.github.com>
2023-08-30 11:14:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,30 @@
 
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
 
 const App = () => {
+    let [item, setItem] = useState('');
+    let [all, setAll] = useState(fruits);
+
+    useEffect(()=>{
+        let update = fruits.filter(val=>(
+            val.includes(item)
+        ))
+        setAll(update)
+    }, [item])
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <h1>Search items</h1>
+        <input type="text" onChange={(e)=>{setItem(e.target.value)}}></input>
+        <ul>
+            {
+                all&& all.map(val=>(
+                    <li>{val}</li>
+                ))
+            }
+        </ul>
     </div>
   )
 }
